c775e89feb7295b477326f4e5d3c0ef7010358d1
CHAN <150508884+zerochani@users.noreply.github.com>
2025-12-31 16:19:23 +0900

    Initial commit

---
+++ src/main/java/com/eatsfine/eatsfine/EatsfineApplication.java
@@ -0,0 +1,13 @@
+package com.eatsfine.eatsfine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsfineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatsfineApplication.class, args);
+    }
+
+}
