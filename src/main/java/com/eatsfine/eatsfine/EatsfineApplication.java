5087c22cac995ad62d377d935219082173ca8f98
CHAN <150508884+zerochani@users.noreply.github.com>
2026-01-01 17:03:36 +0900

    [FEAT]: HealthCheck API 구현 및 테스트 추가

--- src/main/java/com/eatsfine/eatsfine/EatsfineApplication.java
+++ src/main/java/com/eatsfine/eatsfine/EatsfineApplication.java
@@ -3,6 +3,9 @@ package com.eatsfine.eatsfine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class EatsfineApplication {
 
---
+++ src/test/java/com/eatsfine/eatsfine/controller/HealthControllerTest.java
@@ -0,0 +1,40 @@
+package com.eatsfine.eatsfine.controller;
+
+import com.eatsfine.eatsfine.system.deploy.config.DeployProperties;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@WebMvcTest(controllers = HealthController.class)
+@EnableConfigurationProperties(DeployProperties.class)
+@AutoConfigureMockMvc
+@DisplayName("HealthController 중형 테스트: 스프링 MVC와 컨트롤러가 잘 결합하여 동작하는가?")
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("healthCheck : 현재 서버가 살아있다면 200 상태코드와 함께 활성화된 프로필을 문자열로 응답한다")
+    void healthCheckTest() throws Exception {
+        mockMvc
+                .perform(get("/api/v1/deploy/health-check"))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType("text/plain;charset=UTF-8"),
+                        content().string("test") // active profile set to 'test'
+                );
+    }
+}
