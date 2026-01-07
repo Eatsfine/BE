42946753b94815aac0110e8fbe5350ced7b2e3b7
twodo0 <dopal0426@gmail.com>
2026-01-07 18:10:46 +0900

    [FEAT]: BusinessHours 엔티티 초기 세팅

---
+++ src/main/java/com/eatsfine/eatsfine/domain/businesshours/entity/BusinessHours.java
@@ -0,0 +1,41 @@
+package com.eatsfine.eatsfine.domain.businesshours.entity;
+
+import com.eatsfine.eatsfine.domain.businesshours.enums.DayOfWeek;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "business_hours")
+public class BusinessHours {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "store_id", nullable = false)
+    private long storeId;
+
+    @Column(name = "day_of_week", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @Column(name = "open_time", nullable = false)
+    private LocalTime openTime;
+
+    @Column(name = "close_time", nullable = false)
+    private LocalTime closeTime;
+
+    @Column(name = "break_start_time")
+    private LocalTime breakStartTime;
+
+    @Column(name = "break_end_time")
+    private LocalTime breakEndTime;
+
+    @Column(name = "is_closed", nullable = false)
+    private boolean isClosed;
+}
