4d46bda46e9f0346c66d0478bea38da9fc75a6ab
정준영 <wnsdud1158@naver.com>
2026-01-07 16:16:22 +0900

    [FIX]: 불필요한 import문 제거

--- src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingQueryService.java
+++ src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingQueryService.java
@@ -1,15 +1,5 @@
 package com.eatsfine.eatsfine.domain.booking.service;
 
-import com.eatsfine.eatsfine.domain.booking.dto.response.AvailableTableResponse;
-import com.eatsfine.eatsfine.domain.booking.dto.response.BookingDetailResponse;
-import com.eatsfine.eatsfine.domain.booking.dto.response.BookingListResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-
 public interface BookingQueryService {
 
 }
