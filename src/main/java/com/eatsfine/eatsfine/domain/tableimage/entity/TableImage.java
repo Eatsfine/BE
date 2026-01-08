bdd3dcf102754c9ed23c90297d6d7010651ff866
twodo0 <dopal0426@gmail.com>
2026-01-08 20:09:00 +0900

    [REFACTOR]: TableImage Store 연관관계 적용 및 BaseEntity 상속

--- src/main/java/com/eatsfine/eatsfine/domain/tableimage/entity/TableImage.java
+++ src/main/java/com/eatsfine/eatsfine/domain/tableimage/entity/TableImage.java
@@ -1,5 +1,7 @@
 package com.eatsfine.eatsfine.domain.tableimage.entity;
 
+import com.eatsfine.eatsfine.domain.store.entity.Store;
+import com.eatsfine.eatsfine.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,13 +12,14 @@ import lombok.*;
 @Builder
 @Table(name = "table_image")
 
-public class TableImage {
+public class TableImage extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "store_id", nullable = false)
-    private Long storeId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
 
     @Column(name = "table_image_url", nullable = false)
     private String tableImageUrl;
