cff7e892858b5c55c40691f8e7d0610964f4d8a7
twodo0 <dopal0426@gmail.com>
2026-01-08 20:15:26 +0900

    [FEAT]: TableImageRepository 세팅

---
+++ src/main/java/com/eatsfine/eatsfine/domain/tableimage/repository/TableImageRepository.java
@@ -0,0 +1,7 @@
+package com.eatsfine.eatsfine.domain.tableimage.repository;
+
+import com.eatsfine.eatsfine.domain.tableimage.entity.TableImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableImageRepository extends JpaRepository<TableImage, Long> {
+}
