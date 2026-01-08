69cff594344a93b7db6b9afbaeaa52399711468b
twodo0 <dopal0426@gmail.com>
2026-01-08 19:57:07 +0900

    [FEAT]: Store 엔티티 설계

--- src/main/java/com/eatsfine/eatsfine/domain/store/entity/Store.java
+++ src/main/java/com/eatsfine/eatsfine/domain/store/entity/Store.java
@@ -1,11 +1,71 @@
 package com.eatsfine.eatsfine.domain.store.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.eatsfine.eatsfine.domain.region.entity.Region;
+import com.eatsfine.eatsfine.domain.store.enums.Category;
+import com.eatsfine.eatsfine.domain.store.enums.StoreApprovalStatus;
+import com.eatsfine.eatsfine.domain.user.entity.User;
+import com.eatsfine.eatsfine.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
 @Entity
-public class Store {
+public class Store extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
+
+    @Column(name = "store_name", nullable = false)
+    private String storeName;
+
+    @Lob
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "main_image_url", nullable = false)
+    private String mainImageUrl;
+
+    @Builder.Default
+    @Column(name = "rating", precision = 2, scale = 1, nullable = false)
+    private BigDecimal rating = BigDecimal.ZERO;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private Category category;
+
+    @Column(name = "min_price", nullable = false)
+    private int minPrice;
+
+    @Column(name = "max_price", nullable = false)
+    private int maxPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "store_approval_status", nullable = false)
+    private StoreApprovalStatus approvalStatus;
+
+    @Builder.Default
+    @Column(name = "booking_interval_minutes", nullable = false)
+    private int bookingIntervalMinutes = 30;
+
 }
