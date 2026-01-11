6d541f0d46cbea76fb33acb22fb38b88d8dbd00a
정준영 <wnsdud1158@naver.com>
2026-01-11 14:43:36 +0900

    [FEAT] StoreTable 엔티티 기본 설정

--- src/main/java/com/eatsfine/eatsfine/domain/storetable/entity/StoreTable.java
+++ src/main/java/com/eatsfine/eatsfine/domain/storetable/entity/StoreTable.java
@@ -1,11 +1,28 @@
 package com.eatsfine.eatsfine.domain.storetable.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.eatsfine.eatsfine.domain.store.entity.Store;
+import com.eatsfine.eatsfine.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class StoreTable {
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class StoreTable extends BaseEntity {
 
     @Id
+    @GeneratedValue
     private Long id;
+
+    private String tableNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store")
+    private Store store;
+
+    private Integer tableSeats;
+
+
 }
