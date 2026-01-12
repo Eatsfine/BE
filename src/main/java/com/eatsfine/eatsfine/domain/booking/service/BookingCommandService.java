0c155adf023a33ac42445b67b6ccbf6147ea49fa
정준영 <wnsdud1158@naver.com>
2026-01-12 21:44:51 +0900

    [FEAT] : 예약 생성 비즈니스 로직 개발

--- src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingCommandService.java
+++ src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingCommandService.java
@@ -1,5 +1,15 @@
 package com.eatsfine.eatsfine.domain.booking.service;
 
+import com.eatsfine.eatsfine.domain.booking.dto.request.BookingRequestDTO;
+import com.eatsfine.eatsfine.domain.booking.dto.response.BookingResponseDTO;
+import com.eatsfine.eatsfine.domain.user.entity.User;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface BookingCommandService {
+
+    BookingResponseDTO.CreateBookingResultDTO createBooking(User user, Long storeId, BookingRequestDTO.CreateBookingDTO dto);
 
 }
