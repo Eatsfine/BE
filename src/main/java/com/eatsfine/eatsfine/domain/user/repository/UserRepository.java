49605dbe954ee1bdaf32ba4dcefadc9c5361eedd
정준영 <wnsdud1158@naver.com>
2026-01-14 15:14:58 +0900

    [FEAT] : UserRepository 임시 사용

--- src/main/java/com/eatsfine/eatsfine/domain/user/repository/UserRepository.java
+++ src/main/java/com/eatsfine/eatsfine/domain/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.eatsfine.eatsfine.domain.user.repository;
 
-public interface UserRepository {
+import com.eatsfine.eatsfine.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
