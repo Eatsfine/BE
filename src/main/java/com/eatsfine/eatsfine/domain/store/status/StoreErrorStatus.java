4a261c99b41d1ada441bb5ec890625c6ba5a363d
정준영 <wnsdud1158@naver.com>
2026-01-14 17:01:46 +0900

    [FEAT] : StoreErrorStatus 추가

--- src/main/java/com/eatsfine/eatsfine/domain/store/status/StoreErrorStatus.java
+++ src/main/java/com/eatsfine/eatsfine/domain/store/status/StoreErrorStatus.java
@@ -13,7 +13,8 @@ public enum StoreErrorStatus implements BaseErrorCode {
     _STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE404", "해당하는 가게를 찾을 수 없습니다."),
 
     _STORE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_DETAIL404", "가게 상세 정보를 찾을 수 없습니다."),
-    ;
+    _STORE_NOT_OPEN_ON_DAY(HttpStatus.NOT_FOUND,"STORE4041" , "해당 영업시간 정보를 찾을 수 없습니다."),;
+
 
     private final HttpStatus httpStatus;
     private final String code;
