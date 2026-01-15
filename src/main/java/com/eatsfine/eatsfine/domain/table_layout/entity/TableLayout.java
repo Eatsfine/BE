c1e6268897c0b22dc68682ea7172d2af81c9dcac
sonjunkyu <thswnsrb467@naver.com>
2026-01-16 02:16:31 +0900

    [FEAT]: TableLayout @SQLRestriction을 추가하여 soft delete 적용 및 관련 필드 추가

--- src/main/java/com/eatsfine/eatsfine/domain/table_layout/entity/TableLayout.java
+++ src/main/java/com/eatsfine/eatsfine/domain/table_layout/entity/TableLayout.java
@@ -5,7 +5,9 @@ import com.eatsfine.eatsfine.domain.storetable.entity.StoreTable;
 import com.eatsfine.eatsfine.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@SQLRestriction("is_deleted = false")
 @Table(name = "table_layout")
 public class TableLayout extends BaseEntity {
 
@@ -33,6 +36,13 @@ public class TableLayout extends BaseEntity {
 
     @Column(name = "is_active", nullable = false)
     private boolean isActive;
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     @OneToMany(mappedBy = "tableLayout", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<StoreTable> tables = new ArrayList<>();
