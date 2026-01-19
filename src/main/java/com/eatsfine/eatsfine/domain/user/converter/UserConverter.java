830754554a8cfa1d941ca0b41c810d7b41aa8a93
SungMinju <minju2928@naver.com>
2026-01-19 16:10:27 +0900

    [Feature] User 컨버터 구현

--- src/main/java/com/eatsfine/eatsfine/domain/user/converter/UserConverter.java
+++ src/main/java/com/eatsfine/eatsfine/domain/user/converter/UserConverter.java
@@ -1,4 +1,18 @@
 package com.eatsfine.eatsfine.domain.user.converter;
 
+import com.eatsfine.eatsfine.domain.user.dto.UserResponseDto;
+import com.eatsfine.eatsfine.domain.user.entity.User;
+
 public class UserConverter {
+
+    public static UserResponseDto.UserInfoDto toUserInfo(User user) {
+        return UserResponseDto.UserInfoDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickName(user.getNickName())
+                .phoneNumber(user.getPhoneNumber())
+                .profileImage(user.getProfileImage())
+                .build();
+    }
+
 }
