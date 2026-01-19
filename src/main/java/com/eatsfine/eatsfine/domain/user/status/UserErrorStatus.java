af53b10e6dd0d0eee73ae6718507f6d2cb82e218
SungMinju <minju2928@naver.com>
2026-01-19 16:09:03 +0900

    [Feature] JWT 생성

---
+++ src/main/java/com/eatsfine/eatsfine/domain/user/status/UserErrorStatus.java
@@ -0,0 +1,46 @@
+package com.eatsfine.eatsfine.domain.user.status;
+
+import com.eatsfine.eatsfine.global.apiPayload.code.BaseErrorCode;
+import com.eatsfine.eatsfine.global.apiPayload.code.ErrorReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorStatus implements BaseErrorCode {
+
+    // 멤버 관련 에러
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
+    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
+
+    // 토큰 유효 에러
+    INVALID_TOKEN(HttpStatus.NOT_FOUND, "TOKEN4001", "토큰이 없습니다."),
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDto getReason() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .message(message)
+                .code(code)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+
+
+
+    }
---
+++ src/main/java/com/eatsfine/eatsfine/global/config/properties/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.eatsfine.eatsfine.global.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String secret;
+
+}
