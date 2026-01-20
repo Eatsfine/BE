686c1be78149410bf9b5f851259f34d891d8dfd4
sonjunkyu <thswnsrb467@naver.com>
2026-01-21 01:38:40 +0900

    [FEAT]: 테이블 생성 검증 서비스 로직 추가

---
+++ src/main/java/com/eatsfine/eatsfine/domain/storetable/service/StoreTableCommandService.java
@@ -0,0 +1,8 @@
+package com.eatsfine.eatsfine.domain.storetable.service;
+
+import com.eatsfine.eatsfine.domain.storetable.dto.req.StoreTableReqDto;
+import com.eatsfine.eatsfine.domain.storetable.dto.res.StoreTableResDto;
+
+public interface StoreTableCommandService {
+    StoreTableResDto.TableCreateDto createTable(Long storeId, StoreTableReqDto.TableCreateDto dto);
+}
