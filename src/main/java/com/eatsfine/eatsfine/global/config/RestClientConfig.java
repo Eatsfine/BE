f16f24abec36c0b29304dbf740b9902a65eeaac8
twodo0 <dopal0426@gmail.com>
2026-01-23 23:42:48 +0900

    [REFACTOR]: WebClient -> RestClient로 변경

--- src/main/java/com/eatsfine/eatsfine/global/config/RestClientConfig.java
+++ src/main/java/com/eatsfine/eatsfine/global/config/RestClientConfig.java
@@ -2,13 +2,13 @@ package com.eatsfine.eatsfine.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
-public class WebClientConfig {
+public class RestClientConfig {
     @Bean
-    public WebClient businessWebClient(WebClient.Builder builder) {
+    public RestClient businessWebClient(RestClient.Builder builder) {
 
         String baseUrl = "https://api.odcloud.kr/api/nts-businessman/v1";
         // 인코딩 문제 방지 설정
