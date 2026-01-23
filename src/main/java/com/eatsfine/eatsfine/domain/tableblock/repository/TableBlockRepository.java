3fd7b67bd242204c5e788d950abc76d3c05d55fc
sonjunkyu <thswnsrb467@naver.com>
2026-01-23 16:31:40 +0900

    [FEAT]: TableBlockRepository 특정 테이블 차단 내역 조회 메소드 추가

--- src/main/java/com/eatsfine/eatsfine/domain/tableblock/repository/TableBlockRepository.java
+++ src/main/java/com/eatsfine/eatsfine/domain/tableblock/repository/TableBlockRepository.java
@@ -5,8 +5,12 @@ import com.eatsfine.eatsfine.domain.tableblock.entity.TableBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface TableBlockRepository extends JpaRepository<TableBlock, Long> {
     List<TableBlock> findByStoreTableAndTargetDate(StoreTable storeTable, LocalDate targetDate);
+
+    Optional<TableBlock> findByStoreTableAndTargetDateAndStartTime(StoreTable storeTable, LocalDate targetDate, LocalTime startTime);
 }
