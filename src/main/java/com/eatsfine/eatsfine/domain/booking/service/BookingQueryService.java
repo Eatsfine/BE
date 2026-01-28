e53a8922ddfcaeb60a73e663073a70f68089ddb2
정준영 <wnsdud1158@naver.com>
2026-01-28 15:58:14 +0900

    [FEAT] : 예약 조회 Service 개발

--- src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingQueryService.java
+++ src/main/java/com/eatsfine/eatsfine/domain/booking/service/BookingQueryService.java
@@ -2,6 +2,7 @@ package com.eatsfine.eatsfine.domain.booking.service;
 
 import com.eatsfine.eatsfine.domain.booking.dto.request.BookingRequestDTO;
 import com.eatsfine.eatsfine.domain.booking.dto.response.BookingResponseDTO;
+import com.eatsfine.eatsfine.domain.user.entity.User;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,4 +12,6 @@ public interface BookingQueryService {
     BookingResponseDTO.TimeSlotListDTO getAvailableTimeSlots(Long storeId, BookingRequestDTO.GetAvailableTimeDTO dto);
 
     BookingResponseDTO.AvailableTableListDTO getAvailableTables(Long storeId, BookingRequestDTO.GetAvailableTableDTO dto);
+
+    BookingResponseDTO.BookingPreviewListDTO getBookingList(User user, String status, Integer page);
 }
