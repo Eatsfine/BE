9c1adcfe718b86752985ffc53059fee05016cae5
sonjunkyu <thswnsrb467@naver.com>
2026-01-28 19:33:14 +0900

    [FEAT]: 테이블 이미지 업로드 서비스 로직 추가

--- src/main/java/com/eatsfine/eatsfine/domain/storetable/service/StoreTableCommandService.java
+++ src/main/java/com/eatsfine/eatsfine/domain/storetable/service/StoreTableCommandService.java
@@ -2,6 +2,7 @@ package com.eatsfine.eatsfine.domain.storetable.service;
 
 import com.eatsfine.eatsfine.domain.storetable.dto.req.StoreTableReqDto;
 import com.eatsfine.eatsfine.domain.storetable.dto.res.StoreTableResDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface StoreTableCommandService {
     StoreTableResDto.TableCreateDto createTable(Long storeId, StoreTableReqDto.TableCreateDto dto);
@@ -9,4 +10,6 @@ public interface StoreTableCommandService {
     StoreTableResDto.TableUpdateResultDto updateTable(Long storeId, Long tableId, StoreTableReqDto.TableUpdateDto dto);
 
     StoreTableResDto.TableDeleteDto deleteTable(Long storeId, Long tableId);
+
+    StoreTableResDto.UploadTableImageDto uploadTableImage(Long storeId, Long tableId, MultipartFile tableImage);
 }
