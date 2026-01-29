baf00a3cf00b7bd42d47a8af4da62c8b1623cfb0
twodo0 <dopal0426@gmail.com>
2026-01-29 16:48:22 +0900

    [FEAT]: 메뉴 수정 성공 응답코드 추가

--- src/main/java/com/eatsfine/eatsfine/domain/menu/status/MenuSuccessStatus.java
+++ src/main/java/com/eatsfine/eatsfine/domain/menu/status/MenuSuccessStatus.java
@@ -17,8 +17,8 @@ public enum MenuSuccessStatus implements BaseCode {
 
 
     _MENU_CREATE_SUCCESS(HttpStatus.CREATED, "MENU202", "메뉴 생성 성공"),
-
     _MENU_DELETE_SUCCESS(HttpStatus.OK, "MENU2002", "메뉴 삭제 성공"),
+    _MENU_UPDATE_SUCCESS(HttpStatus.OK, "MENU2003", "메뉴 수정 성공"),
 
     ;
 
