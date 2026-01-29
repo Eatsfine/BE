9943956e12c08a9c4cb295a4e350a87aeb2b5d07
SungMinju <minju2928@naver.com>
2026-01-30 01:11:34 +0900

    [Feat] 약관동의 추가

---
+++ src/main/java/com/eatsfine/eatsfine/domain/term/entity/Term.java
@@ -0,0 +1,38 @@
+package com.eatsfine.eatsfine.domain.term.entity;
+
+import com.eatsfine.eatsfine.domain.user.entity.User;
+import com.eatsfine.eatsfine.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "term")
+public class Term extends BaseEntity {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @OneToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", nullable = false, unique = true)
+        private User user;
+
+        @Builder.Default
+        @Column(name = "tos_consent", nullable = false)
+        private Boolean tosConsent = true;
+
+        @Builder.Default
+        @Column(name = "privacy_consent", nullable = false)
+        private Boolean privacyConsent = true;
+
+
+        @Column(name = "marketing_consent", nullable = false)
+        private Boolean marketingConsent;
+
+}
