6f2dbffde0d722524bfc1676e87942e0088c8959
CHAN <150508884+zerochani@users.noreply.github.com>
2026-02-05 13:57:38 +0900

    [FIX]: 테스트 환경 설정 및 의존성 주입 오류로 인한 빌드 실패 수정

--- src/test/java/com/eatsfine/eatsfine/controller/HealthControllerTest.java
+++ src/test/java/com/eatsfine/eatsfine/controller/HealthControllerTest.java
@@ -1,12 +1,16 @@
 package com.eatsfine.eatsfine.controller;
 
+import com.eatsfine.eatsfine.global.auth.CustomAccessDeniedHandler;
+import com.eatsfine.eatsfine.global.auth.CustomAuthenticationEntryPoint;
 import com.eatsfine.eatsfine.global.config.DeployProperties;
+import com.eatsfine.eatsfine.global.config.jwt.JwtAuthenticationFilter;
 import com.eatsfine.eatsfine.global.controller.HealthController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,12 +24,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @WebMvcTest(controllers = HealthController.class)
 @EnableConfigurationProperties(DeployProperties.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @DisplayName("HealthController 중형 테스트: 스프링 MVC와 컨트롤러가 잘 결합하여 동작하는가?")
 class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @MockBean
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @MockBean
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Test
     @DisplayName("healthCheck : 현재 서버가 살아있다면 200 상태코드와 함께 활성화된 프로필을 문자열로 응답한다")
