6849f674c1f335ceaf8f48f4dc27823b2df51b36
정준영 <wnsdud1158@naver.com>
2026-02-07 22:10:45 +0900

    [FEAT] : BookingErrorStatus 추가

--- src/main/java/com/eatsfine/eatsfine/domain/booking/status/BookingErrorStatus.java
+++ src/main/java/com/eatsfine/eatsfine/domain/booking/status/BookingErrorStatus.java
@@ -21,7 +21,8 @@ public enum BookingErrorStatus implements BaseErrorCode {
     _ALREADY_CANCELED(HttpStatus.BAD_REQUEST,"BOOKING4004", "이미 취소된 예약입니다."),
     _TABLE_NOT_FOUND(HttpStatus.NOT_FOUND, "TABLE404", "해당 테이블을 찾을 수 없습니다."),
     _TABLE_SEATS_NOT_FOUND(HttpStatus.NOT_FOUND, "TABLE4041", "테이블 좌석 정보를 찾을 수 없습니다."),
-    _INVALID_BOOKING_ACCESS(HttpStatus.FORBIDDEN, "BOOKING403", "해당 가게의 예약이 아닙니다.")
+    _INVALID_BOOKING_ACCESS(HttpStatus.FORBIDDEN, "BOOKING403", "해당 가게의 예약이 아닙니다."),
+    _BOOKING_NOT_USER(HttpStatus.FORBIDDEN, "BOOKING4031", "해당 유저의 예약이 아닙니다.");
     ;
 
     private final HttpStatus httpStatus;
